3afa9834be65a435b07913219dbc408a0abe3225
navyxie <navyxie2010@gmail.com>
2016-06-28 13:47:40 +0800

    发送post请求需要使用form表单格式

--- test/index.js
+++ test/index.js
@@ -3,6 +3,9 @@ var BAOFOO = require('../lib/index');
 var pri_key_str = 'MIICeAIBADANBgkqhkiG9w0BAQEFAASCAmIwggJeAgEAAoGBAPI7Hb+2X0/1r4+xJXBoFRgFSN9G4xSSvlfKFd6ynYpHklLLfNG6z21bw+DDWvrkNpcbAjAzfgMZAXmBJKQRTMQM9RBxu2978ot1KJJBfk1Njw1lWrYCZiQ1uaNhXykHrBHYiBxDE/uVSAEmQ9IJyxB++khsNe7WWT2re2JW+OzfAgMBAAECgYEAz5OLRbth6CKRJODYRYBb+y6kKPoVJI8v4AlEPofv6wy0PpE0UIH2uS83J0ghkfi5Mzoo4OdvZ/Yoxle9738HuRrDVYzDgsr2mGmr11ESVhe7jmdrPryURai0qedjpSVgfY9QrQsgOilPR+GYkkOh0szhwkm4KHIKx8bdxpHvHCECQQD+IGFGNHov+3/HQJmcuqLRUizEbsi4mkKhiqcInQ482o+7uoysVYjszIkAlpDXVlxMqPEeVn0p9rVChJxiBMlxAkEA9ARJECnNP+y6eePT6ueii10p1wajUjcxL30bqYLPVWuhSICmttCoNiApIj4HFQSQBxDZXoSODEuo9m3TWJFzTwJBAN7wXSYP55msk36jx59dhHUKGEgDwIdinU3Gq5682b69JxdUIxEUwNis3wvrCwo+sx51n4Iz8f4cdwvx9pdvB6ECQQDd9Lfwt9U2fEHydUVhumijk45nRGZydjmLFKWAvreQ32HI7Ry31XvsH7zKpNkUSR4pDy5pRvFeRcPew28mdMcJAkAPW9D4FCgJAfH1v1be8YkP44sJ4nRlcm03hOx8/Ebp/Xx+lqHNsf0RIg+vO9PuD7vEm3ZAz5UXe4TamX6HwMxH';
 var pub_key_str = 'MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCpBL+SzNNpeJVAhP8XO91dPrQHf6w2k7aqwnWGjnYjRlmy7iaeUJT/wb3PoH+ioGNTJyPHvfYHalEmvi133K3N8NdHizaYTjm1hgNBhBIK8aPZCR76C3UtQPv4mqA5fTZEWnRbr8JrVJoFNEN2+4JQwCeDtaC3OzPPflETt6zdOQIDAQAB';
 var baofoo;
+var acc_no = '6227003320240000000';
+var bind_id = '201603261412121000009649074';
+var mobile = '13800000000';
 describe('BAOFOO', function() {
     this.timeout(50000);
     it('instance not ok', function() {
@@ -26,15 +29,17 @@ describe('BAOFOO', function() {
         baofoo.bindCard({
             trans_serial_no: 'koala-trans_serial_no' + Date.now(),
             trans_id: 'koala-trans_id' + Date.now(),
-            acc_no: '6227003320240000000',
+            acc_no: acc_no,
             id_card: '440882198105421458',
             id_holder: '李四',
-            mobile: '13800000000',
+            mobile: mobile,
             pay_code: 'CCB',
             additional_info: '测试',
-            req_reserved: '保留字段'
+            req_reserved: '保留字段',
+            sms_code: '123456'
         }, function(err, data) {
-            data.should.have.properties(['code'])
+            data.should.have.properties({'code': '0000'});
+            should.exists(data.data.bind_id);
             done(err);
         });
     });
@@ -50,7 +55,7 @@ describe('BAOFOO', function() {
             trans_id: 'koala-trans_id' + Date.now(),
             bind_id: 'test-bind_id'
         }, function(err, data) {
-            data.should.have.properties(['code'])
+            data.should.have.properties({'code': '0000'});
             done(err);
         });
     });
@@ -64,9 +69,10 @@ describe('BAOFOO', function() {
         baofoo.queryBindCard({
             trans_serial_no: 'koala-trans_serial_no' + Date.now(),
             trans_id: 'koala-trans_id' + Date.now(),
-            acc_no: '6227003320240000000'
+            acc_no: acc_no
         }, function(err, data) {
-            data.should.have.properties(['code'])
+            data.should.have.properties({'code': '0000'});
+            data.data.bind_id.should.be.equal(bind_id);
             done(err);
         });
     });
@@ -80,7 +86,7 @@ describe('BAOFOO', function() {
         baofoo.doPay({
             trans_serial_no: 'koala-trans_serial_no' + Date.now(),
             trans_id: 'koala-trans_id' + Date.now(),
-            bind_id: 'test-bind_id',
+            bind_id: bind_id,
             txn_amt: 1,
             sms_code: '1234'
         }, function(err, data) {
@@ -98,10 +104,10 @@ describe('BAOFOO', function() {
         baofoo.sendMessage({
             trans_serial_no: 'koala-trans_serial_no' + Date.now(),
             trans_id: 'koala-trans_id' + Date.now(),
-            bind_id: 'test-bind_id',
+            bind_id: bind_id,
             txn_amt: 1,
-            mobile: '13800000000',
-            acc_no: '6227003320240000000',
+            mobile: mobile,
+            acc_no: acc_no,
             next_txn_sub_type: '04'
         }, function(err, data) {
             data.should.have.properties(['code'])
